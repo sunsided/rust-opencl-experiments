87a271e51b6add5ee8e951accc813fbd4c0f9d8b
Markus Mayer <widemeadows@gmail.com>
2023-01-12 17:35:19 +0100

    Use local array to improve caching

--- bins/opencl_bf_search/src/opencl/dot_product.rs
+++ bins/opencl_bf_search/src/opencl/dot_product.rs
@@ -7,10 +7,16 @@ const DOT_PRODUCT_SOURCE: &'static str = "
                              __global float *result,
                              const unsigned int cols) {
         int row = get_global_id(0);
+
+        // TODO: Ensure dims is less than 4096.
+        __local float local_vec[4096];
+        event_t copy_event = async_work_group_copy(local_vec, vector, cols, 0);
+        wait_group_events(1, &copy_event);
+
         float dot = 0.f;
         // TODO: get_global_size(1)?
         for (int i = 0; i < cols; i++) {
-            dot += matrix[row*cols + i] * vector[i];
+            dot += matrix[row*cols + i] * local_vec[i];
         }
         result[row] = dot;
     }";
