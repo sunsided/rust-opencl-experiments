a503655a7205968866aef7ff1b1d6dc6a2e14557
Markus Mayer <widemeadows@gmail.com>
2023-01-19 23:10:27 +0100

    Use aligned memory allocation for memory chunk

--- crates/memchunk/src/memory_chunk.rs
+++ crates/memchunk/src/memory_chunk.rs
@@ -1,43 +1,29 @@
 use crate::topk::{topk, Entry};
 use abstractions::{NumDimensions, NumVectors};
+use alloc_madvise::Memory;
 
 #[derive(Debug)]
 pub struct MemoryChunk {
     num_vecs: usize,
     virt_num_vecs: usize,
     num_dims: usize,
-    data: Vec<f32>,
+    data: Memory,
 }
 
 impl MemoryChunk {
-    pub fn new_from(data: Vec<f32>, num_vectors: NumVectors) -> Self {
-        let num_dims = data.len() / *num_vectors;
-        assert_eq!(
-            num_dims % 16,
-            0,
-            "Number of dimensions must be a multiple of 16"
-        );
-        assert_eq!(
-            data.len(),
-            *num_vectors * num_dims,
-            "Data size does not match vector count and inferred dimensionality"
-        );
-        Self {
-            data,
-            num_vecs: *num_vectors,
-            virt_num_vecs: *num_vectors,
-            num_dims,
-        }
-    }
-
     pub fn new(num_vectors: NumVectors, num_dimensions: NumDimensions) -> Self {
         assert_eq!(
             *num_dimensions % 16,
             0,
             "Number of dimensions must be a multiple of 16"
         );
+
+        let num_elems = num_vectors * num_dimensions;
+        let num_bytes = num_elems * std::mem::size_of::<f32>();
+        let chunk = Memory::allocate(num_bytes, true, true).expect("memory allocation failed");
+
         Self {
-            data: vec![0.0; num_vectors * num_dimensions],
+            data: chunk,
             num_vecs: *num_vectors,
             virt_num_vecs: *num_vectors,
             num_dims: *num_dimensions,
@@ -57,7 +43,7 @@ impl MemoryChunk {
 
         let mut results = vec![0.0; num_vecs];
 
-        let data = &self.data;
+        let data: &[f32] = self.data.as_ref();
         for (v, result) in results.iter_mut().enumerate() {
             let start_index = v * num_dims;
 
@@ -80,7 +66,7 @@ impl MemoryChunk {
 
         let mut results = vec![0.0; num_vecs];
 
-        let data = &self.data;
+        let data: &[f32] = self.data.as_ref();
         for (v, result) in results.iter_mut().enumerate() {
             let start_index = v * num_dims;
 
@@ -103,7 +89,7 @@ impl MemoryChunk {
 
         let mut results = vec![0.0; num_vecs];
 
-        let data = &self.data;
+        let data: &[f32] = self.data.as_ref();
         for (v, result) in results.iter_mut().enumerate() {
             let start_index = v * num_dims;
 
@@ -139,7 +125,8 @@ impl MemoryChunk {
         let start = idx * self.num_dims;
         let end = (idx + 1) * self.num_dims;
         debug_assert!(idx < self.data.len());
-        &self.data[start..end]
+        let data: &[f32] = self.data.as_ref();
+        &data[start..end]
     }
 
     pub fn len(&self) -> usize {
@@ -167,19 +154,30 @@ impl MemoryChunk {
     pub fn double(&mut self) {
         self.num_vecs *= 2;
         self.virt_num_vecs *= 2;
-        let mut cloned = self.data.clone();
-        self.data.append(&mut cloned);
+
+        let num_elems = self.num_dims * self.num_vecs;
+        let num_bytes = num_elems * std::mem::size_of::<f32>();
+        let mut chunk = Memory::allocate(num_bytes, true, true).expect("memory allocation failed");
+
+        let src: &[f32] = self.data.as_ref();
+        let dest: &mut [f32] = chunk.as_mut();
+        dest[..src.len()].copy_from_slice(src);
+        dest[src.len()..].copy_from_slice(src);
+
+        self.data = chunk;
     }
 }
 
 impl AsRef<[f32]> for MemoryChunk {
     fn as_ref(&self) -> &[f32] {
-        &self.data[..self.num_dims * self.virt_num_vecs]
+        let data: &[f32] = self.data.as_ref();
+        &data[..self.num_dims * self.virt_num_vecs]
     }
 }
 
 impl AsMut<[f32]> for MemoryChunk {
     fn as_mut(&mut self) -> &mut [f32] {
-        &mut self.data[..self.num_dims * self.virt_num_vecs]
+        let data: &mut [f32] = self.data.as_mut();
+        &mut data[..self.num_dims * self.virt_num_vecs]
     }
 }
